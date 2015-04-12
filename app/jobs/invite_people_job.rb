cb3347bbadc8d0fbd2dde4123844c5758bfb3859
sungpi <sungpia@me.com>
2015-04-12 04:36:58 -0500

    fix

--- app/jobs/invite_people_job.rb
+++ app/jobs/invite_people_job.rb
@@ -4,14 +4,16 @@ class InvitePeopleJob < ActiveJob::Base
   def perform(group, vote_id)
 		admin_user_name = User.find(group.user_id).name
 		group.users.each do |user|
-			puts user
-			user.devices.each do |device|
-				puts device.push_id
-				InvitePeoplePushJob.perform_now(device.push_id,
-				                                admin_user_name,
-				                                group.id,
-				                                vote_id
-				)
+			if group.user_id != user.id
+				puts user
+				user.devices.each do |device|
+					puts device.push_id
+					InvitePeoplePushJob.perform_now(device.push_id,
+					                                admin_user_name,
+					                                group.id,
+					                                vote_id
+					)
+				end
 			end
 		end
   end
