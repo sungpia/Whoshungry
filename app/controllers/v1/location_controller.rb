06f346c56f2f9947d6041d691896f65ad1c525b4
sungpi <sungpia@me.com>
2015-04-15 01:46:34 -0500

    fix

--- app/controllers/v1/location_controller.rb
+++ app/controllers/v1/location_controller.rb
@@ -13,7 +13,9 @@ module V1
 			@locations["locations"] = []
 			@locations["user_id"]
 			users.each do |x|
-				@locations["locations"] << x.locations.last
+				if Rsvp.where(user: @user, vote: vote)[0] != nil && Rsvp.find_by(user: @user, vote: vote).rsvp == 1
+ 				  @locations["locations"] << x.locations.last
+				end
 			end
 			render json: @locations, status: 200
 		end
