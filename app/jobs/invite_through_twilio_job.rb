dbe2d817ddf48c9690ce40e0dfeeae7d137206b2
sungpi <sungpia@me.com>
2015-04-27 23:17:28 -0500

    fix

--- app/jobs/invite_through_twilio_job.rb
+++ app/jobs/invite_through_twilio_job.rb
@@ -4,7 +4,7 @@ class InviteThroughTwilioJob < ActiveJob::Base
 
   def perform(*args)
     contact = args[0]
-    invite_message = args[1] + " Invited you to eat! Download Who's Hungry? at http://whoshungryapp.com"
+    invite_message = args[1] + " Invited you out to eat! Download \"Who's Hungry?\" at http://whoshungryapp.com"
 # put your own credentials here
 	  account_sid = 'AC4f3f27e9fe0992c071939c49ab56ad0f'
 	  auth_token = 'b925a47e99be6e9500707d0a7a0c18ac'
